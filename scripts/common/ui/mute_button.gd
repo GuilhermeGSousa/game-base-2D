33b11faefc128b282062a4dac926d5dda753415a
Guilherme Sousa <guilherme.sousa1994@gmail.com>
2023-04-21 09:42:54 +0200

    Added mute button to pause menu

---
+++ scripts/common/ui/mute_button.gd
@@ -0,0 +1,12 @@
+extends Button
+
+@onready var master_bus = AudioServer.get_bus_index('Master')
+
+func _enter_tree():
+	toggled.connect(toggle_mute)
+	
+func _exit_tree():
+	toggled.disconnect(toggle_mute)
+	
+func toggle_mute(is_pressed : bool):
+	AudioServer.set_bus_mute(master_bus, is_pressed)
